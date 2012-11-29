b3d19b654901bf82a0243c7198b9b50ecab9a237
dmotz <motzdc@gmail.com>
2012-11-29 01:22:00 -0500

    added new demo

--- demo/demo.js
+++ demo/demo.js
@@ -11,6 +11,8 @@ function init(){
         domi4 = new OriDomi(demo4, { hPanels: 1, vPanels: 6, perspective: 500 }),
         demo5 = document.getElementsByClassName('demo5')[0],
         domi5 = new OriDomi(demo5, { hPanels: 10, vPanels: 1 }),
+        demo6 = document.getElementsByClassName('demo6')[0],
+        domi6 = new OriDomi(demo6, { hPanels: 1, vPanels: 5, touchEnabled: false, speed: 400 }),
         foldMe = document.querySelector('.fold-me > p'),
         foldDomi = new OriDomi(foldMe, { vPanels: 1, hPanels: 4, perspective: 200, speed: 500 });
 
@@ -32,6 +34,11 @@ function init(){
         domi4.accordion(-50);
         domi5.curl(-60, 1);
     }, 3000);
+    
+    
+    demo6.addEventListener('click', function(){
+        domi6.isFoldedUp ? domi6.reset() : domi6.foldUp();
+    }, false);
 
 }
 
